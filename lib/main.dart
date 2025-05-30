82949a8818c06425995be203d94cc97c8dedaf1b
mohajuwa <mohaghram2019@gmail.com>
2025-05-30 03:02:25 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:ecom_modwir/constants.dart';
+import 'package:ecom_modwir/controllers/menu_app_controller.dart';
+import 'package:ecom_modwir/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Amper Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuAppController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
